8a2b38ad5926715b2a3a76b14be9778acbb6f144
jewelteves <121958695+jewelteves@users.noreply.github.com>
2023-12-11 14:33:41 +0800

    Update driverModel.js

--- server/model/driverModel.js
+++ server/model/driverModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    bodyno: {type: Number, require: true},
+    bodyno: {type: String, require: true},
     fullname: {type: String, require: true},
     address: {type: String, require: true},
     contactno: {type: String, require: true},
