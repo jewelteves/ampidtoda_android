e768afffe0a478dd4730b39570e3e36bbde465fa
jewelteves <121958695+jewelteves@users.noreply.github.com>
2023-12-14 13:52:14 +0800

    Update driverModel.js

--- server/model/driverModel.js
+++ server/model/driverModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    bodyno: {type: String, require: true},
+    bodyno: {type: Number, require: true},
     fullname: {type: String, require: true},
     address: {type: String, require: true},
-    contactno: {type: String, require: true},
+    gcashno: {type: Number, require: true},
+    gcashname: {type: String, require: true},
+    contactno: {type: Number, require: true},
     password: {type: String, require: true},
 })
 const Crudapi = mongoose.model('driver', schema);
